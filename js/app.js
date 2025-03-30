ddefb0988931307bb78b82c905169e55b9ecfffa
SonrisitaEventos <57836093+SonrisitaEventos@users.noreply.github.com>
2025-03-30 00:14:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -308,8 +308,8 @@ const mensajes = [
 setInterval(cambiarMensaje, 15000);
 // Chat mensajes
   const URL_CHAT = "https://script.google.com/macros/s/AKfycbwcCHRjjj00h4qi-kvxw3woxkgUvqovlGQb-SRWsECinKV5mqFh2PRegw28wuDj9RCk/exec";
-   const sheetURL = "https://opensheet.elk.sh/1FKHc3KV4rIym0WX1K_zOSPRydVfUi3TT7KyxJz4fyXc/Hoja%201";
-
+   const sheetURL = "https://opensheet.elk.sh/1UJOht6wP2mNGFm1edP6mrn0ylUYdXEBbV2r8R6PR-n0/Hoja%201";
+   
   function enviarMensajeChat() {
     const nombre = document.getElementById("nombreChat").value.trim();
     const mensaje = document.getElementById("mensajeChat").value.trim();
